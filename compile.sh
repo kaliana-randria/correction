c3cc3026d26aa8f6c15030b4a1af0c6f5663a4da
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-07 15:17:01 +0300

    [develop][compile] ajout compile.sh

---
+++ compile.sh
@@ -0,0 +1,9 @@
+mvn clean compile
+mvn spring-boot:run
+
+
+# mvn clean
+# mvn clean package
+
+
+# mvn clean compile exec:java -Dexec.mainClass="com.example.correction.CorrectionApplication" -DskipTests
