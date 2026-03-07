0314956525135cbf09c90e06a05acb7f7cb56b67
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-07 15:31:07 +0300

    [crud-candidat][fonctionnalite] creation class sur candidat

---
+++ src/main/java/com/example/correction/entity/Candidat.java
@@ -0,0 +1,64 @@
+package com.example.correction.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "candidat")
+public class Candidat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nom", nullable = false)
+    private String nom;
+
+    @Column(name = "prenom", nullable = false)
+    private String prenom;
+
+    @Column(name = "dtn", nullable = false)
+    private String dtn;
+
+    @Column(name = "sexe", nullable = false)
+    private String sexe;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getDtn() {
+        return dtn;
+    }
+
+    public void setDtn(String dtn) {
+        this.dtn = dtn;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+}
