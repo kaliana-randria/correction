e5539f7a3045996eceb525974cb75b6d4dd96643
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-07 13:20:08 +0300

    [controller][update] MatiereController.java

--- src/main/java/com/example/correction/controller/MatiereController.java
+++ src/main/java/com/example/correction/controller/MatiereController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -55,4 +56,22 @@ public class MatiereController {
         
         return mv;
     }
+
+    @GetMapping("/matiere/supprimer/{id}")
+    public ModelAndView deleteMatiere(@PathVariable int id,
+                                       RedirectAttributes redirectAttributes) {
+        ModelAndView mv = new ModelAndView("redirect:/matiere/list");
+        
+        try {
+            matiereService.deleteById(id);
+            redirectAttributes.addFlashAttribute("success", "Matiere supprimé avec succès !");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Erreur lors de la suppression : " + e.getMessage());
+        }
+        
+        return mv;
+    }
+
+    
+
 }
