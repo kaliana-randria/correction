61f40adbfd30e6ac612596504bdf52e8085aa094
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-07 14:31:46 +0300

    [crud-matiere][configuration] update

--- src/main/java/com/example/correction/config/WebConfig.java
+++ src/main/java/com/example/correction/config/WebConfig.java
@@ -1,17 +1,13 @@
 package com.example.correction.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan("com.example.correction")
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
