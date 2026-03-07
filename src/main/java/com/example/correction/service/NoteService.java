48b2f6c74fa5c78609e6ca3ae192159450ccf2fd
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-07 19:18:37 +0300

    [crud-note][fonctionnalite] update class

--- src/main/java/com/example/correction/service/NoteService.java
+++ src/main/java/com/example/correction/service/NoteService.java
@@ -1,8 +1,31 @@
 package com.example.correction.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.correction.entity.Note;
+import com.example.correction.repository.NoteRepository;
 
 @Service
 public class NoteService {
-    
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public List<Note> findAll() {
+        return noteRepository.findAll();
+    }
+
+    public Note findById(int id) {
+        return noteRepository.findById(id).orElse(null);
+    }
+
+    public Note save(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public void deleteById(int id) {
+        noteRepository.deleteById(id);
+    }
 }
