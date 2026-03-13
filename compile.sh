8d983fcef83b6d25bd3e23ec5824654307817b09
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-13 21:33:40 +0300

    [sprint-2][configuration] configuration base

--- compile.sh
+++ compile.sh
@@ -7,3 +7,10 @@ mvn spring-boot:run
 
 
 # mvn clean compile exec:java -Dexec.mainClass="com.example.correction.CorrectionApplication" -DskipTests
+
+
+# ================= Lancement
+
+SPRING_PROFILES_ACTIVE=sprint1 ./mvnw spring-boot:run
+ou
+SPRING_PROFILES_ACTIVE=sprint2 ./mvnw spring-boot:run
