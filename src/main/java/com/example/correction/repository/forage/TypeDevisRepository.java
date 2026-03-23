fb20ef5540ac14beeaa0d51fa779a6fd3136dcc2
kaliana-randria <189259948+kaliana-randria@users.noreply.github.com>
2026-03-23 13:52:39 +0300

    [sprint-3][typeDevis] creation class + fonction

---
+++ src/main/java/com/example/correction/repository/forage/TypeDevisRepository.java
@@ -0,0 +1,10 @@
+package com.example.correction.repository.forage;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.correction.entity.forage.TypeDevis;
+
+@Repository
+public interface TypeDevisRepository extends JpaRepository<TypeDevis, Integer>{
+}
